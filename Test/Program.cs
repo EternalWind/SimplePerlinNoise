5c0bc66cf3699c70377290c99b4f30873363e8f1
EternalWind <eternalwind@live.cn>
2015-05-05 20:57:24 +0900

    Cleaned up a little bit

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,4 @@
-﻿//#define DOUBLE_DEPTH
+﻿#define DOUBLE_DEPTH
 
 using System;
 using System.Collections.Generic;
@@ -8,10 +8,11 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Diagnostics;
 
 using SimplePerlinNoise;
 
-namespace SimpleNoiseTest
+namespace Test
 {
     class Program
     {
@@ -28,16 +29,166 @@ namespace SimpleNoiseTest
             return data;
         }
 
-        static double HighPass(double value, double threshold)
+        static double[] NormaliseNoise(double[] non_uniform_noise)
         {
-            if (value < threshold)
-                return 0.0f;
+            double max = non_uniform_noise[0];
+            double min = non_uniform_noise[0];
+
+            foreach (double n in non_uniform_noise)
+            {
+                if (n > max)
+                    max = n;
+                if (n < min)
+                    min = n;
+            }
+
+            double range = max - min;
+
+            for (int i = 0; i < non_uniform_noise.Length; ++i)
+                non_uniform_noise[i] = (non_uniform_noise[i] - min) / range;
+
+            return non_uniform_noise;
+        }
+
+        static double MiddlePass(double val, double min, double max, double min_displacing_val = 0.0, double max_displacing_val = 1.0)
+        {
+            if (val > max)
+                return max_displacing_val;
+            else if (val < min)
+                return min_displacing_val;
             else
-                return value;
+                return val;
+        }
+
+        static double MiddleCut(double val, double min, double max, double mid_displacing_val)
+        {
+            if (val > max || val < min)
+                return val;
+            else
+                return mid_displacing_val;
+        }
+
+        static double Contrast(double val, double min, double max, double min_displacing_val = 0.0, double max_displacing_val = 1.0)
+        {
+            double result = MiddlePass(val, min, max, min_displacing_val, max_displacing_val);
+
+            result = (result - min_displacing_val) / (max_displacing_val - min_displacing_val);
+
+            return result;
+        }
+
+        static double[,] Perturb(double[,] vals, Noise2D noiseX, Noise2D noiseY, int displace_range)
+        {
+            int x = vals.GetLength(0);
+            int y = vals.GetLength(1);
+
+            double[,] temp_vals = new double[x, y];
+            for (int i = 0; i < x; ++i)
+            {
+                for (int j = 0; j < y; ++j)
+                {
+                    double n1 = noiseX.EvalAt((double)i / (x - 1), (double)j / (y - 1));
+                    double n2 = noiseY.EvalAt((double)i / (x - 1), (double)j / (y - 1));
+
+                    int u = i + (int)(n1 * displace_range);
+                    int v = j + (int)(n2 * displace_range);
+
+                    if (u < 0)
+                        u = 0;
+                    else if (u >= x)
+                        u = x - 1;
+
+                    if (v < 0)
+                        v = 0;
+                    else if (v >= y)
+                        v = y - 1;
+
+                    temp_vals[i, j] = vals[u, v];
+                }
+            }
+
+            return temp_vals;
+        }
+
+        static double[,] Erode(double[,] vals, double smoothness)
+        {
+            int x = vals.GetLength(0);
+            int y = vals.GetLength(1);
+
+            for (int i = 1; i < x - 1; ++i)
+            {
+                for (int j = 1; j < y - 1; ++j)
+                {
+                    double max = 0.0;
+                    int max_u = 0;
+                    int max_v = 0;
+
+                    if (vals[i, j] < 0.5)
+                    {
+                        for (int u = -1; u <= 1; ++u)
+                        {
+                            for (int v = -1; v <= 1; ++v)
+                            {
+                                if (u != 0 || v != 0)
+                                {
+                                    var diff = vals[i, j] - vals[i + u, j + v];
+                                    if (Math.Abs(diff) > max)
+                                    {
+                                        max = diff;
+                                        max_u = u;
+                                        max_v = v;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (max != 0 && Math.Abs(max) <= (smoothness / x))
+                    {
+                        double fix = max / 2;
+                        vals[i, j] -= fix;
+                        vals[i + max_u, j + max_v] += fix;
+                    }
+                }
+            }
+
+            return vals;
+        }
+
+        static double[,] Smoothen(double[,] vals)
+        {
+            int x = vals.GetLength(0);
+            int y = vals.GetLength(1);
+
+            for (int i = 1; i < x - 1; ++i)
+            {
+                for (int j = 1; j < y - 1; ++j)
+                {
+                    if (vals[i, j] < 0.6)
+                    {
+                        double total = 0.0;
+
+                        for (int u = -1; u <= 1; ++u)
+                        {
+                            for (int v = -1; v <= 1; ++v)
+                            {
+                                total += vals[i + u, j + v];
+                            }
+                        }
+
+                        vals[i, j] = total / 9;
+                    }
+                }
+            }
+
+            return vals;
         }
 
         static void Main(string[] args)
         {
+            var stop_watch = new Stopwatch();
+
+            stop_watch.Start();
             var settings = ConfigurationManager.AppSettings;
 
             int dimensions = 2;
@@ -51,6 +202,14 @@ namespace SimpleNoiseTest
             int seed = 1;
             if (!Int32.TryParse(settings["Random_Seed"], out seed))
                 Console.WriteLine("Failed to read 'Random_Seed' from the config file.");
+
+            int iterations = 1;
+            if (!Int32.TryParse(settings["Iterations"], out iterations))
+                Console.WriteLine("Failed to read 'Iterations' from the config file.");
+
+            double persistence = 108;
+            if (!Double.TryParse(settings["Persistence"], out persistence))
+                Console.WriteLine("Failed to read 'Persistence' from the config file.");
 
             int source_x = 256;
             if (!Int32.TryParse(settings["Source_X"], out source_x))
@@ -78,23 +237,46 @@ namespace SimpleNoiseTest
 
             string output_file = settings["Output_File_Name"];
 
-            double plane_threshold = 0.0;
-            if (!Double.TryParse(settings["Plane_Threshold"], out plane_threshold))
-                Console.WriteLine("Failed to read 'Plane_Threshold' from the config file.");
-
-            var begin = DateTime.Now;
+            Console.WriteLine("Time for reading settings: " + stop_watch.ElapsedMilliseconds);
+            stop_watch.Restart();
 
             var generator = new NoiseGenerator();
-            generator.Initialize(dimensions, base_gradient_count, seed);
+            generator.Initialize(seed);
 
-            var noise = generator.Generate2D(source_x, source_y, source_offset_x, source_offset_y);
+            //var noise = generator.GenerateComplex2D(iterations, persistence, source_x, source_y, source_offset_x, source_offset_y);
+            Console.WriteLine("Time for initialization: " + stop_watch.ElapsedMilliseconds);
 
-            Bitmap preview_output = new Bitmap(output_x, output_y, PixelFormat.Format32bppRgb);
+            //var noise2 = generator.GenerateComplex2D(iterations, persistence, source_x, source_y, source_offset_x + 1, source_offset_y);
+
+            //int offset_x = (int)((output_x - 1) * (double)1 / (source_x - 1));
+
+            //for (int j = 0; j < output_y; ++j)
+            //{
+            //    double sampling_pos_y = (double)j / (output_y - 1);
+
+            //    for (int i = 0; i < output_x - offset_x; ++i)
+            //    {
+            //        double sampling_pos_x2 = (double)(i) / (output_x - 1);
+            //        double sampling_pos_x1 = sampling_pos_x2 + 1.0 / (source_x);
+
+            //        var r1 = noise.EvalAt(sampling_pos_x1, sampling_pos_y);
+            //        var r2 = noise2.EvalAt(sampling_pos_x2, sampling_pos_y);
+
+            //        if (r1 != r2)
+            //        {
+            //            Console.WriteLine("Diff at (" + i + ", " + j + ")! r1 = " + r1 + " r2 = " + r2);
+            //        }
+            //    }
+            //}
+
+            //Bitmap output_preview = new Bitmap(output_x, output_y);
 #if DOUBLE_DEPTH
             PixelFormat format = PixelFormat.Format16bppRgb565;
 #else
             PixelFormat format = PixelFormat.Format8bppIndexed;
 #endif
+
+            Bitmap preview_output = new Bitmap(output_x, output_y, PixelFormat.Format32bppRgb);
 
             Bitmap output = new Bitmap(output_x, output_y, format);
             Rectangle rect = new Rectangle(0, 0, output_x, output_y);
@@ -112,7 +294,46 @@ namespace SimpleNoiseTest
 #endif
 
             System.Runtime.InteropServices.Marshal.Copy(ptr, raw_memory, 0, bytes);
-          
+
+            stop_watch.Restart();
+
+            double[,] non_uniform_noise = new double[output_x, output_y];
+
+            for (int j = 0; j < output_y; ++j)
+            {
+                double sampling_pos_y = (double)j / (output_y - 1);
+
+                for (int i = 0; i < output_x; ++i)
+                {
+                    double sampling_pos_x = (double)i / (output_x - 1);
+                    non_uniform_noise[i, j] = (generator.Generate2D((double)sampling_pos_x * 4, (double)sampling_pos_y * 4) + 1.0) / 2.0;
+                    //non_uniform_noise[i, j] = Contrast(non_uniform_noise[i, j], 0.5, 0.7, 0.5, 0.7);
+                    //non_uniform_noise[pixel_idx] = MiddleCut(non_uniform_noise[pixel_idx], 0.4, 0.7, 0.55);
+                    //non_uniform_noise[pixel_idx] = MiddleCut(non_uniform_noise[pixel_idx], 0.2, 0.3, 0.25);
+                }
+            }
+
+            Console.WriteLine("Time for generating noise: " + stop_watch.ElapsedMilliseconds + " (" + Stopwatch.Frequency + ")");
+            Console.WriteLine("Generation profiling: ");
+            Console.WriteLine("Time for calculating gradients ---- " + generator.TimeForCalculatingGradients);
+            Console.WriteLine("Time for calculating dot products ---- " + generator.TimeForCalculatingDotProducts);
+            Console.WriteLine("Time for calculating interpolations ---- " + generator.TimeForCalculatingInterpolations);
+
+            stop_watch.Restart();
+
+            //int frequency = 32;
+            //non_uniform_noise = Perturb(non_uniform_noise, generator.Generate2D(frequency, frequency), generator.Generate2D(frequency, frequency, frequency, frequency), 32);
+
+            //for (int i = 0; i < 10; ++i)
+            //{
+            //    non_uniform_noise = Erode(non_uniform_noise, 16.0);
+            //}
+
+            //non_uniform_noise = Smoothen(non_uniform_noise);
+
+            Console.WriteLine("Time for post processing: " + stop_watch.ElapsedMilliseconds);
+            stop_watch.Stop();
+
             for (int j = 0; j < output_y; ++j)
             {
                 double sampling_pos_y = (double)j / output_y;
@@ -120,10 +341,9 @@ namespace SimpleNoiseTest
                 for (int i = 0; i < output_x; ++i)
                 {
                     double sampling_pos_x = (double)i / output_x;
+                    int pixel_idx = j * output_y + i;
 
-                    double normalised_sampling_result = (noise.EvalAt(sampling_pos_x, sampling_pos_y) + 1) / 2;
-                    normalised_sampling_result = HighPass(normalised_sampling_result, plane_threshold);
-
+                    double normalised_sampling_result = non_uniform_noise[i, j];
                     int sampling_result = (int)(max_depth * normalised_sampling_result);
                     int preview_sampling_result = (int)(255 * normalised_sampling_result);
 
@@ -131,9 +351,6 @@ namespace SimpleNoiseTest
                         throw new Exception("The sampling result is larger than 1!");
 
                     preview_output.SetPixel(i, j, Color.FromArgb(preview_sampling_result, preview_sampling_result, preview_sampling_result));
-
-                    int pixel_idx = j * output_y + i;
-
 #if DOUBLE_DEPTH
                     raw_memory[pixel_idx] = (short)sampling_result;
 #else
@@ -154,9 +371,6 @@ namespace SimpleNoiseTest
                 fs.Write(raw_bytes, 0, raw_bytes.Length);
             }
 
-            var end = DateTime.Now;
-
-            Console.WriteLine("Done! Time used: " + (end - begin).TotalMilliseconds + "ms");
             Console.ReadKey();
         }
     }
